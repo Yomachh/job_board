16d22398e54fbcd8fb76bf3afbf4d5831503b4d8
Yomachh <yomach@YomachYoga.localdomain>
2021-08-12 18:59:21 +0200

    baza i formatiranje

--- job_board_scrapy/job_board_scrapy/spiders/mojposao.py
+++ job_board_scrapy/job_board_scrapy/spiders/mojposao.py
@@ -1,115 +1,140 @@
 import scrapy
 from scrapy import Request
 from peewee import *
+import datetime
 
-db = SqliteDatabase('mojposao.db')
+db = SqliteDatabase("poslovi.db")
+
 
 class MojPosao(Model):
 
-    db_zupanija = TextField()
-    db_mjesto = TextField()
-    db_tvrtka = TextField()
-    db_pozicija = TextField()
-    db_vrijedi_do = TextField()
-    db_link_za_posao = TextField()
+    zupanija = TextField()
+    mjesto = TextField()
+    tvrtka = TextField()
+    pozicija = TextField()
+    vrijedi_do = TextField()
+    link_za_posao = TextField()
+    datum_prikupljanja = DateField()
 
     class Meta:
         database = db
 
+
 db.connect()
 db.create_tables([MojPosao])
 
+
 class MojposaoSpider(scrapy.Spider):
 
-    name = 'mojposao'
-    allowed_domains = ['www.moj-posao.net']
+    name = "mojposao"
+    allowed_domains = ["www.moj-posao.net"]
 
     def start_requests(self):
-        url = 'https://www.moj-posao.net/Zupanije/'
+        url = "https://www.moj-posao.net/Zupanije/"
         yield Request(url=url, callback=self.parse_start_url)
 
     def parse_start_url(self, response):
         info = {
-            'zupanije':  response.selector.xpath("//div[@class='list']/ul/li/a/text()").extract(),
-            'link_zup':  response.selector.xpath("//div[@class='list']/ul/li/a/@href").extract()
-            }
-        for zup, link in zip(info['zupanije'], info['link_zup']):
+            "zupanije": response.selector.xpath(
+                "//div[@class='list']/ul/li/a/text()"
+            ).extract(),
+            "link_zup": response.selector.xpath(
+                "//div[@class='list']/ul/li/a/@href"
+            ).extract(),
+        }
+        for zup, link in zip(info["zupanije"], info["link_zup"]):
             yield Request(
-                url=link,
-                callback=self.parse,
-                cb_kwargs=dict(zupanije=zup[6:])
+                url=link, callback=self.parse, cb_kwargs=dict(zupanije=zup[6:])
             )
 
-
     def parse(self, response, zupanije):
-        for featured_job in response.selector.xpath('//*[@id="featured-jobs"]/ul/li'):
-            pozicije = featured_job.xpath('.//*[@class="job-position  "]/text()').extract()
-            mjesta = featured_job.xpath('.//*[@class="job-location"]/text()').extract()
-            vrijede_do = featured_job.xpath('.//*[@class="deadline"]/text()').extract()
-            linkovi_za_poslove = featured_job.xpath('.//*[@class="job-data"]/a/@href').extract()
+        j_datum_prikupljanja = datetime.date.today()
 
-            for pozicija, mjesta, vrijedi_do, link_za_posao in zip(pozicije, mjesta, vrijede_do, linkovi_za_poslove):
-                tvrtka = featured_job.xpath('.//img[@class="logo"]/@title').extract()[0]
-                pozicija = pozicija.strip()
+        for featured_job in response.selector.xpath('//*[@id="featured-jobs"]/ul/li'):
+            pozicije = featured_job.xpath(
+                './/*[@class="job-position  "]/text()'
+            ).extract()
+            j_mjesta = featured_job.xpath(
+                './/*[@class="job-location"]/text()'
+            ).extract()
+            vrijede_do = featured_job.xpath('.//*[@class="deadline"]/text()').extract()
+            linkovi_za_poslove = featured_job.xpath(
+                './/*[@class="job-data"]/a/@href'
+            ).extract()
+
+            for j_pozicija, j_mjesta, j_vrijedi_do, j_link_za_posao in zip(
+                pozicije, j_mjesta, vrijede_do, linkovi_za_poslove
+            ):
+                j_tvrtka = featured_job.xpath('.//img[@class="logo"]/@title').extract()[
+                    0
+                ]
+                j_pozicija = j_pozicija.strip()
 
                 MojPosao.create(
-                    db_zupanija=zupanije,
-                    db_mjesto=mjesta,
-                    db_tvrtka=tvrtka,
-                    db_pozicija=pozicija,
-                    db_link_za_posao=link_za_posao,
-                    db_vrijedi_do=vrijedi_do
-                    )
+                    zupanija=zupanije,
+                    mjesto=j_mjesta,
+                    tvrtka=j_tvrtka,
+                    pozicija=j_pozicija,
+                    link_za_posao=j_link_za_posao,
+                    vrijedi_do=j_vrijedi_do,
+                    datum_prikupljanja=j_datum_prikupljanja
+                )
 
                 yield {
-                    'Zupanija': zupanije,
-                    'Mjesto': mjesta,
-                    'Tvrtka': tvrtka,
-                    'Pozicija': pozicija,
-                    'Vrijedi_do': vrijedi_do,
-                    'Link_za_posao': link_za_posao
+                    "Zupanija": zupanije,
+                    "Mjesto": j_mjesta,
+                    "Tvrtka": j_tvrtka,
+                    "Pozicija": j_pozicija,
+                    "Vrijedi_do": j_vrijedi_do,
+                    "Link_za_posao": j_link_za_posao,
                 }
-
 
         for job in response.selector.xpath('//*[@class="general-info"]'):
             pozicije = job.xpath('.//*[@class="job-title"]/a/text()').extract()
-            for pozicija in pozicije:
-                pozicija = pozicija.strip()
-                mjesto = job.xpath('.//*[@class="job-location"]/text()').extract()
-                vrijeme = job.xpath('.//*[@class="deadline"]/time/text()').extract()
-                link = job.xpath('.//*[@class="job-title"]/a/@href').extract()
-                tvrtka = job.xpath('.//*[@class="job-company"]/a/text()').extract() or job.xpath('.//*[@class="job-company"]/text()').extract()
+            for j_pozicija in pozicije:
+                j_pozicija = j_pozicija.strip()
+                j_mjesto = job.xpath('.//*[@class="job-location"]/text()').extract()
+                j_vrijeme = job.xpath('.//*[@class="deadline"]/time/text()').extract()
+                j_link = job.xpath('.//*[@class="job-title"]/a/@href').extract()
+                j_tvrtka = (
+                    job.xpath('.//*[@class="job-company"]/a/text()').extract()
+                    or job.xpath('.//*[@class="job-company"]/text()').extract()
+                )
 
-                if type(mjesto) == list:
-                    mjesto = mjesto[0]
-                if type(tvrtka) == list:
-                    tvrtka = tvrtka[0]
-                if type(vrijeme) == list:
-                    vrijeme = vrijeme[0]
-                if type(link) == list:
-                    link = link[0]
+                if type(j_mjesto) == list:
+                    j_mjesto = j_mjesto[0]
+                if type(j_tvrtka) == list:
+                    j_tvrtka = j_tvrtka[0]
+                if type(j_vrijeme) == list:
+                    j_vrijeme = j_vrijeme[0]
+                if type(j_link) == list:
+                    j_link = j_link[0]
 
                 yield {
-                    'Zupanija': zupanije,
-                    'Mjesto':  mjesto,
-                    'Tvrtka': tvrtka,
-                    'Pozicija': pozicija,
-                    'Vrijedi_do': vrijeme,
-                    'Link_za_posao': link
-                    }
+                    "Zupanija": zupanije,
+                    "Mjesto": j_mjesto,
+                    "Tvrtka": j_tvrtka,
+                    "Pozicija": j_pozicija,
+                    "Vrijedi_do": j_vrijeme,
+                    "Link_za_posao": j_link,
+                }
 
                 MojPosao.create(
-                    db_zupanija=zupanije,
-                    db_mjesto=mjesto,
-                    db_tvrtka=tvrtka,
-                    db_pozicija=pozicija,
-                    db_vrijedi_do=vrijeme,
-                    db_link_za_posao=link
-                    )
+                    zupanija=zupanije,
+                    mjesto=j_mjesto,
+                    tvrtka=j_tvrtka,
+                    pozicija=j_pozicija,
+                    vrijedi_do=j_vrijeme,
+                    link_za_posao=j_link,
+                    datum_prikupljanja=j_datum_prikupljanja
+                )
 
-
-        for next_page in response.selector.xpath('*//*[@class="next icon"]/a/@href').extract():
-            yield Request(url=next_page, callback=self.parse, cb_kwargs=dict(zupanije=zupanije))
+        for next_page in response.selector.xpath(
+            '*//*[@class="next icon"]/a/@href'
+        ).extract():
+            yield Request(
+                url=next_page, callback=self.parse, cb_kwargs=dict(zupanije=zupanije)
+            )
 
 
 db.close()
--- job_board_scrapy/job_board_scrapy/spiders/posao.py
+++ job_board_scrapy/job_board_scrapy/spiders/posao.py
@@ -3,9 +3,10 @@ from scrapy import Request
 from peewee import *
 import datetime
 
-db = SqliteDatabase('posao_hr.db')
+db = SqliteDatabase("poslovi.db")
 
-class Posao(Model):
+
+class PosaoHr(Model):
 
     zupanija = TextField()
     mjesto = TextField()
@@ -13,48 +14,54 @@ class Posao(Model):
     pozicija = TextField()
     vrijedi_do = TextField()
     link_za_posao = TextField()
+    datum_prikupljanja = DateField()
 
     class Meta:
         database = db
 
+
 db.connect()
-db.create_tables([Posao])
+db.create_tables([PosaoHr])
 
 
 class PosaoSpider(scrapy.Spider):
 
-    name = 'posao'
-    allowed_domains = ['www.posao.hr']
-
+    name = "posao"
+    allowed_domains = ["www.posao.hr"]
 
     def start_requests(self):
-        url = 'https://www.posao.hr'
+        url = "https://www.posao.hr"
         yield Request(url=url, callback=self.parse_start_url)
-
 
     def parse_start_url(self, response):
         info = {
-            'zupanije':  response.selector.xpath('//*[@class="districts"]/ul/li/a/text()').extract(),
-            'link_zup':  response.selector.xpath('//*[@class="districts"]/ul/li/a/@href').extract()
-            }
-        for zup, link in zip(info['zupanije'], info['link_zup']):
+            "zupanije": response.selector.xpath(
+                '//*[@class="districts"]/ul/li/a/text()'
+            ).extract(),
+            "link_zup": response.selector.xpath(
+                '//*[@class="districts"]/ul/li/a/@href'
+            ).extract(),
+        }
+        for zup, link in zip(info["zupanije"], info["link_zup"]):
             yield Request(
                 url=link,
                 callback=self.parse,
-                cb_kwargs=dict(zupanije=zup, first_page=True)
-                )
-
+                cb_kwargs=dict(zupanije=zup, first_page=True),
+            )
 
     def parse(self, response, zupanije, first_page):
-        for special_job in  response.selector.xpath('//div[@class="intro"]'):
-            sj_mjesto = special_job.xpath('./a/div/div/span/text()').extract()[0]
-            sj_tvrtka = special_job.xpath('../div/a/img/@alt').extract()[0]
-            sj_pozicija = special_job.xpath('./a/strong/text()').extract()
-            sj_link_za_posao = special_job.xpath('./a/@href').extract()
+        j_datum_prikupljanja = datetime.date.today()
+        for special_job in response.selector.xpath('//div[@class="intro"]'):
+            sj_mjesto = special_job.xpath("./a/div/div/span/text()").extract()[0]
+            sj_tvrtka = special_job.xpath("../div/a/img/@alt").extract()[0]
+            sj_pozicija = special_job.xpath("./a/strong/text()").extract()
+            sj_link_za_posao = special_job.xpath("./a/@href").extract()
 
-            dani_do_isteka = special_job.xpath('./a/div/div/span/text()').extract()[1]
+            dani_do_isteka = special_job.xpath("./a/div/div/span/text()").extract()[1]
             try:
-                sj_vrijedi_do_posao = datetime.date.today() + datetime.timedelta(days=int((dani_do_isteka.split()[0])))
+                sj_vrijedi_do_posao = datetime.date.today() + datetime.timedelta(
+                    days=int((dani_do_isteka.split()[0]))
+                )
             except:
                 pass
             if type(sj_pozicija) == list:
@@ -62,62 +69,100 @@ class PosaoSpider(scrapy.Spider):
             if type(sj_link_za_posao) == list:
                 sj_link_za_posao = sj_link_za_posao[0]
 
-            Posao.create(
+            PosaoHr.create(
                 zupanija=zupanije,
                 mjesto=sj_mjesto,
                 tvrtka=sj_tvrtka,
-                pozicija= sj_pozicija,
+                pozicija=sj_pozicija,
                 link_za_posao=sj_link_za_posao,
-                vrijedi_do=sj_vrijedi_do_posao
-                )
+                vrijedi_do=sj_vrijedi_do_posao,
+                datum_prikupljanja=j_datum_prikupljanja,
+            )
 
             yield {
-                'Zupanija': zupanije,
-                'Mjesto': sj_mjesto,
-                'Tvrtka': sj_tvrtka,
-                'Pozicija': sj_pozicija,
-                'Vrijedi_do': sj_vrijedi_do_posao,
-                'Link_za_posao': sj_link_za_posao
-                }
+                "Zupanija": zupanije,
+                "Mjesto": sj_mjesto,
+                "Tvrtka": sj_tvrtka,
+                "Pozicija": sj_pozicija,
+                "Vrijedi_do": sj_vrijedi_do_posao,
+                "Link_za_posao": sj_link_za_posao,
+            }
 
         if first_page == True:
-            j_tvrtke = response.selector.xpath('//table//div[@class="employer res_date_time_comp"]/text()').extract() +  response.selector.xpath('//table//div[@class="company employer res_date_time_comp"]/text()').extract()
-            j_pozicije = response.selector.xpath('//table//div[@class="title"]/text()').extract()  +  response.selector.xpath('//table//div[@class="normal_title title"]/text()').extract()
+            j_tvrtke = (
+                response.selector.xpath(
+                    '//table//div[@class="employer res_date_time_comp"]/text()'
+                ).extract()
+                + response.selector.xpath(
+                    '//table//div[@class="company employer res_date_time_comp"]/text()'
+                ).extract()
+            )
+            j_pozicije = (
+                response.selector.xpath('//table//div[@class="title"]/text()').extract()
+                + response.selector.xpath(
+                    '//table//div[@class="normal_title title"]/text()'
+                ).extract()
+            )
         else:
-            j_tvrtke = response.selector.xpath('//table//div[@class="employer res_date_time_comp"]/text()').extract() or  response.selector.xpath('//table//div[@class="company employer res_date_time_comp"]/text()').extract()
-            j_pozicije = response.selector.xpath('//table//div[@class="title"]/text()').extract()  or  response.selector.xpath('//table//div[@class="normal_title title"]/text()').extract()
+            j_tvrtke = (
+                response.selector.xpath(
+                    '//table//div[@class="employer res_date_time_comp"]/text()'
+                ).extract()
+                or response.selector.xpath(
+                    '//table//div[@class="company employer res_date_time_comp"]/text()'
+                ).extract()
+            )
+            j_pozicije = (
+                response.selector.xpath('//table//div[@class="title"]/text()').extract()
+                or response.selector.xpath(
+                    '//table//div[@class="normal_title title"]/text()'
+                ).extract()
+            )
 
-        j_mjesta = response.selector.xpath('//table//div[@class="location res_date_time_comp"]/text()').extract()
-        j_datumi = response.selector.xpath('//table//div[@class="deadline res_date_time_comp"]/text()').extract()
-        j_linkovi_za_poslove = response.selector.xpath('//table//a/@href').extract()
+        j_mjesta = response.selector.xpath(
+            '//table//div[@class="location res_date_time_comp"]/text()'
+        ).extract()
+        j_datumi = response.selector.xpath(
+            '//table//div[@class="deadline res_date_time_comp"]/text()'
+        ).extract()
+        j_linkovi_za_poslove = response.selector.xpath("//table//a/@href").extract()
 
-        for j_mjesto, j_tvrtka, j_pozicija, j_datum, j_link in zip(j_mjesta, j_tvrtke, j_pozicije, j_datumi, j_linkovi_za_poslove):
+        for j_mjesto, j_tvrtka, j_pozicija, j_datum, j_link in zip(
+            j_mjesta, j_tvrtke, j_pozicije, j_datumi, j_linkovi_za_poslove
+        ):
             try:
-                j_datum = datetime.date.today() + datetime.timedelta(days=int((j_datum.split()[0])))
+                j_datum = datetime.date.today() + datetime.timedelta(
+                    days=int((j_datum.split()[0]))
+                )
             except:
                 pass
             if type(j_pozicija) == list:
                 j_pozicija = j_pozicija[0]
             if type(j_link) == list:
-                j_link= j_link[0]
+                j_link = j_link[0]
 
-            Posao.create(
+            PosaoHr.create(
                 zupanija=zupanije,
                 mjesto=j_mjesto,
                 tvrtka=j_tvrtka,
-                pozicija= j_pozicija,
+                pozicija=j_pozicija,
                 link_za_posao=j_link,
-                vrijedi_do=j_datum
-                )
+                vrijedi_do=j_datum,
+                datum_prikupljanja=j_datum_prikupljanja,
+            )
 
             yield {
-                'Zupanija': zupanije,
-                'Mjesto': j_mjesto,
-                'Tvrtka': j_tvrtka,
-                'Pozicija': j_pozicija,
-                'Vrijedi_do': j_datum,
-                'Link_za_posao': j_link
-                }
+                "Zupanija": zupanije,
+                "Mjesto": j_mjesto,
+                "Tvrtka": j_tvrtka,
+                "Pozicija": j_pozicija,
+                "Vrijedi_do": j_datum,
+                "Link_za_posao": j_link,
+            }
 
         next_page = response.selector.xpath('*//span[@class="pages"]/a/@href').extract()
-        yield Request(url=next_page[-2], callback=self.parse, cb_kwargs=dict(zupanije=zupanije, first_page=False))
+        yield Request(
+            url=next_page[-2],
+            callback=self.parse,
+            cb_kwargs=dict(zupanije=zupanije, first_page=False),
+        )
